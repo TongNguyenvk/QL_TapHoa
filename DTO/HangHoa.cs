4944159c87156c8c9e4552ce335c3accf0c7723c
TongNguyenvk <tongct08@gmail.com>
2025-10-19 19:45:02 +0700

    chore: Thêm solution file và tài liệu thiết kế

---
+++ DTO/HangHoa.cs
@@ -0,0 +1,14 @@
+﻿namespace TapHoa.DTO
+{
+    public class HangHoa
+    {
+        public int MaHang { get; set; }
+        public string TenHang { get; set; }
+        public int MaLoaiHang { get; set; }
+        public string TenLoaiHang { get; set; }
+        public int SoLuong { get; set; }
+        public decimal GiaNhap { get; set; }
+        public decimal GiaBan { get; set; }
+        public string MoTa { get; set; }
+    }
+}
